8f687cc5650694c6896d3db993729717eb1bc8b3
wangh1998 <1418613097@qq.com>
2022-06-22 11:30:44 +0800

    xxx

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Date: 2022-06-22 11:27:55
+ * @LastEditTime: 2022-06-22 11:30:11
+ * @FilePath: /wangh1998.github.io/vue.config.js
+ */
+module.exports = {
+  publicPath: './',
+}
